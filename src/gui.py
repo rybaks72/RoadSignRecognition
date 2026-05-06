b10c434470a98cd03a6aef2604b7f29b9bfea8e1
Jakub Szostak <01189011@pw.edu.pl>
2026-05-06 11:59:19 +0200

    dry

--- src/gui.py
+++ src/gui.py
@@ -3,36 +3,8 @@ from tkinter import filedialog, Label, Button, Frame
 from PIL import Image, ImageTk
 import torch
 from torchvision import transforms
-import torch.nn as nn
-import torch.nn.functional as F
 import os
-
-# Define the model class
-class MultiScaleCNN(nn.Module):
-    def __init__(self, num_classes=43):
-        super(MultiScaleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=5)
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=5)
-        self.fc1 = nn.Linear(64*5*5 + 128*1*1, 1024)
-        self.fc2 = nn.Linear(1024, num_classes)
-        self.dropout = nn.Dropout(0.5)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.pool1(x)
-        x = F.relu(self.conv2(x))
-        x = self.pool2(x)
-        feat1 = x.view(-1, 64*5*5)
-        x = F.relu(self.conv3(x))
-        feat2 = x.view(-1, 128*1*1)
-        combined = torch.cat((feat1, feat2), dim=1)
-        x = F.relu(self.fc1(combined))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
+from model import MultiScaleCNN
 
 # Define classes
 classes = {
